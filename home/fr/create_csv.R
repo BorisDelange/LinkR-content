277cc19688e4523ed0b850b0f147eed907e848d1
Boris Delange <boris.delange@gmail.com>
2023-06-05 19:56:51 +0200

    Update fr get started

--- home/fr/create_csv.R
+++ home/fr/create_csv.R
@@ -13,37 +13,42 @@ tibble::tribble(
   ## Cards
   "home", "card", "overview", "overview", "", "2023-05-29_overview.Md", 1L, "2023-05-29 08:00:00",
   
-  "home", "card", "news", "news1", "News 1", "", 1L, "2023-04-10 08:00:00",
-  "home", "card", "news", "news2", "News 2", "", 2L, "2023-04-10 08:00:00",
+  #"home", "card", "news", "news1", "News 1", "", 1L, "2023-04-10 08:00:00",
+  #"home", "card", "news", "news2", "News 2", "", 2L, "2023-04-10 08:00:00",
  
   
   # 2) Get started
   
   ## Pivot_items
+  "get_started", "pivot_item", "default_data", "default_data", "Données par défaut", "", 1L, "2023-06-05 00:00:00",
+  "get_started", "pivot_item", "first_study", "first_study", "Votre première étude", "", 1L, "2023-06-05 00:00:00",
+  "get_started", "pivot_item", "concepts_mapping", "concepts_mapping", "Alignement de concepts", "", 1L, "2023-06-05 00:00:00",
   
   ## Cards
- 
+  "get_started", "card", "concepts_mapping", "div1", "Terminologies & concepts", "2023-06-05_concepts_mapping_div1.Md", 1L, "2023-06-05 00:00:00",
+  "get_started", "card", "concepts_mapping", "div2", "Alignement de concepts dans LinkR", "", 2L, "2023-06-05 00:00:00",
+  "get_started", "card", "concepts_mapping", "div3", "Exporter une terminologie", "", 3L, "2023-06-05 00:00:00",
   
   # 3) Tutorials
   
   ## Pivot_items
-  "tutorials", "pivot_item", "health_data_warehouses", "health_data_warehouses", "Entrepôts de données de santé", "", 1L, "2023-04-10 08:00:00",
+  # "tutorials", "pivot_item", "health_data_warehouses", "health_data_warehouses", "Entrepôts de données de santé", "", 1L, "2023-04-10 08:00:00",
   
   ## Cards
-  "tutorials", "card", "health_data_warehouses", "div1", "Div 1", "", 1L, "2023-04-10 08:00:00",
+  # "tutorials", "card", "health_data_warehouses", "div1", "Div 1", "", 1L, "2023-04-10 08:00:00",
   
   
   # 4) Resources
   
   ## Pivot_items
-  "resources", "pivot_item", "learn_r", "learn_r", "Programmer en R", "", 2L, "2023-04-10 08:00:00",
-  "resources", "pivot_item", "health_data_warehouses", "health_data_warehouses", "Entrepôts de données de santé", "", 1L, "2023-04-10 08:00:00",
-  "resources", "pivot_item", "learn_sql", "learn_sql", "Programmer en SQL", "", 3L, "2023-04-10 08:00:00",
-  "resources", "pivot_item", "learn_python", "learn_python", "Programmer en Python", "", 4L, "2023-04-10 08:00:00",
+  # "resources", "pivot_item", "learn_r", "learn_r", "Programmer en R", "", 2L, "2023-04-10 08:00:00",
+  # "resources", "pivot_item", "health_data_warehouses", "health_data_warehouses", "Entrepôts de données de santé", "", 1L, "2023-04-10 08:00:00",
+  # "resources", "pivot_item", "learn_sql", "learn_sql", "Programmer en SQL", "", 3L, "2023-04-10 08:00:00",
+  # "resources", "pivot_item", "learn_python", "learn_python", "Programmer en Python", "", 4L, "2023-04-10 08:00:00",
   
   ## Cards
-  "resources", "card", "health_data_warehouses", "div1", "Div 1", "", 2L, "2023-04-10 08:00:00",
-  "resources", "card", "health_data_warehouses", "div2", "Div 2", "", 1L, "2023-04-10 08:00:00"
+  # "resources", "card", "health_data_warehouses", "div1", "Div 1", "", 2L, "2023-04-10 08:00:00",
+  # "resources", "card", "health_data_warehouses", "div2", "Div 2", "", 1L, "2023-04-10 08:00:00"
   
 ) %>%
   readr::write_csv("home/fr/tabs_and_cards.csv")
