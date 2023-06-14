a50b8111ca1233cdeebbdc62a0e692ed20e72acd
Boris Delange <boris.delange@gmail.com>
2023-06-14 16:27:06 +0200

    Remote git plugin test

--- plugins/aggregated/4ca7fe8f141d98fb88ab645c0b07d2388087803742c90d1d53d67d22a34e5029/ui.R
+++
@@ -1,7 +0,0 @@
-tagList(
-    shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 20),
-        shiny.fluent::TextField.shinyInput(ns("text_input_%widget_id%")),
-        shiny.fluent::PrimaryButton.shinyInput(ns("submit_%widget_id%"), i18np$t("show"))
-    ), br(),
-    div(verbatimTextOutput(ns("text_output_%widget_id%")), style = "border:dashed 1px;")
-)
