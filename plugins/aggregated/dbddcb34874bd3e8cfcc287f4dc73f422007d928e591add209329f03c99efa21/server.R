c799108d22d5eb9a17735e162b058faf0437cc03
Boris Delange <boris.delange@gmail.com>
2023-08-01 12:51:23 +0200

    Update ggplot2 plugin

--- plugins/aggregated/dbddcb34874bd3e8cfcc287f4dc73f422007d928e591add209329f03c99efa21/server.R
+++ plugins/aggregated/dbddcb34874bd3e8cfcc287f4dc73f422007d928e591add209329f03c99efa21/server.R
@@ -1,72 +1,210 @@
 # Update x & y variables dropdowns
-x_variables <- convert_tibble_to_list(selected_concepts, key_col = "concept_id", text_col = "concept_name")
+concepts <- tibble::tibble(concept_id = 0L, concept_name = i18np$t("none")) %>% dplyr::bind_rows(selected_concepts %>% dplyr::select(concept_id, concept_name))
+x_variables <- convert_tibble_to_list(concepts, key_col = "concept_id", text_col = "concept_name")
 shiny.fluent::updateDropdown.shinyInput(session, "x_variable_%widget_id%", options = x_variables)
-y_variables <- convert_tibble_to_list(selected_concepts, key_col = "concept_id", text_col = "concept_name")
+y_variables <- convert_tibble_to_list(concepts, key_col = "concept_id", text_col = "concept_name")
 shiny.fluent::updateDropdown.shinyInput(session, "y_variable_%widget_id%", options = y_variables)
+
+# List of inputs (to save & get saved params)
+
+dropdowns <- c("plot_function", "plot_theme", "stat", "bins_type", "x_variable", "y_variable", "colour_pal", "group_by", "summarize_fct")
+textfields <- c("x_label", "y_label")
+spin_buttons <- c("num_of_bins", "bin_width", "group_by_num")
+toggle_inputs <- "group_data"
+colour_inputs <- "colour"
+inputs <- c(dropdowns, textfields, spin_buttons, toggle_inputs, colour_inputs)
 
 # Render plot
 observeEvent(input$show_%widget_id%, {
     %req%
     
-    # A x variable must be selected
-    req(length(input$x_variable_%widget_id%) > 0)
+    data <- list()
+    variable <- list()
     
-    x_data <- tibble::tibble()
+    data$x <- tibble::tibble()
+    variable$x <- tibble::tibble()
+    data$y <- tibble::tibble()
+    variable$y <- tibble::tibble()
     
-    # Get x variable in data
-    x_variable <- d$dataset_all_concepts %>% dplyr::filter(concept_id_1 == input$x_variable_%widget_id%)
-    req(nrow(x_variable) > 0)
+    # Get x & y variables in data
+    if (length(input$x_variable_%widget_id%) > 0) variable$x <- d$dataset_all_concepts %>% dplyr::filter(concept_id_1 == input$x_variable_%widget_id%)
+    if (length(input$y_variable_%widget_id%) > 0) variable$y <- d$dataset_all_concepts %>% dplyr::filter(concept_id_1 == input$y_variable_%widget_id%)
     
-    x_variable$domain_id <- tolower(x_variable$domain_id)
-    if (x_variable$domain_id %in% c("observation", "measurement")) x_data <- d[[x_variable$domain_id]] %>% 
-        dplyr::filter(get(paste0(x_variable$domain_id, "_concept_id")) == x_variable$concept_id_1)
+    if (nrow(variable$x) > 0){
+        variable$x$domain_id <- tolower(variable$x$domain_id)
+        if (variable$x$domain_id %in% c("observation", "measurement")) data$x <- d[[tolower(variable$x$domain_id)]] %>% 
+            dplyr::filter(get(paste0(tolower(variable$x$domain_id), "_concept_id")) == variable$x$concept_id_1)
+    }
     
-    req(nrow(x_data) > 0)
+    if (nrow(variable$y) > 0){
+        variable$y$domain_id <- tolower(variable$y$domain_id)
+        if (variable$y$domain_id %in% c("observation", "measurement")) data$y <- d[[tolower(variable$y$domain_id)]] %>% 
+            dplyr::filter(get(paste0(tolower(variable$y$domain_id), "_concept_id")) == variable$y$concept_id_1)
+    }
     
     # Render plot
     output$plot_output_%widget_id% <- renderPlot({
+        %req%
     
         figure <- ggplot2::ggplot()
-    
-        if (isolate(input$plot_function_%widget_id%) == "geom_histogram"){
-            figure <- x_data %>% ggplot2::ggplot(ggplot2::aes(x = value_as_number))
-            
-            if (isolate(input$bins_type_%widget_id%) == "num_of_bins") figure <- figure + 
-                ggplot2::geom_histogram(bins = isolate(input$num_of_bins_%widget_id%), fill = isolate(input$x_colour_%widget_id%), color = "#FFFFFF")
-            else if (isolate(input$bins_type_%widget_id%) == "bin_width") figure <- figure + 
-                ggplot2::geom_histogram(binwidth = isolate(input$bin_width_%widget_id%), fill = isolate(input$x_colour_%widget_id%), color = "#FFFFFF")
+        
+        # If we choose to group data
+        if (isolate(input$group_data_%widget_id%)){
+            for(var in c("x", "y")){
+                if (nrow(data[[var]]) > 0){
                 
-            figure <- figure +
-                do.call(getFromNamespace(isolate(input$plot_theme_%widget_id%), "ggplot2"), list()) +
-                ggplot2::labs(x = isolate(input$x_label_%widget_id%), y = isolate(input$y_label_%widget_id%))
+                    # Group by person_id
+                    if (isolate(input$group_by_%widget_id% == "person_id")) data[[var]] <- data[[var]] %>% dplyr::group_by(person_id)
+                    
+                    # Group by datetime
+                    if (isolate(input$group_by_%widget_id%) == "datetime"){
+                        data[[var]] <- data[[var]]  %>%
+                            dplyr::filter(!is.na(value_as_number)) %>%
+                            dplyr::mutate(datetime_rounded = lubridate::floor_date(
+                                !!rlang::sym(paste0(tolower(variable$x$domain_id), "_datetime")), 
+                                unit = paste0(isolate(input$group_by_num_%widget_id%), " ", isolate(input$group_by_type_%widget_id%)))) %>%
+                            dplyr::group_by(datetime_rounded)
+                    }
+                    
+                    # Summarize with selected summarize function
+                    data[[var]] <- 
+                        data[[var]] %>% 
+                        dplyr::summarize(value_as_number = match.fun(isolate(input$summarize_fct_%widget_id%))(value_as_number, na.rm = TRUE)) %>%
+                        dplyr::ungroup() 
+                    
+                    # Rename datetime col with original name
+                    if (isolate(input$group_by_%widget_id%) == "datetime") data[[var]] <- data[[var]] %>% 
+                        dplyr::rename(!!paste0(tolower(variable$x$domain_id), "_datetime") := datetime_rounded)
+                }
+            }
+        }
+    
+        # HISTOGRAM
+        
+        if (isolate(input$plot_function_%widget_id%) == "geom_histogram"){
+            req(nrow(data$x) > 0)
+        
+            # Create ggplot2 figure
+            figure <- data$x %>% ggplot2::ggplot(ggplot2::aes(x = value_as_number))
+            
+            # Add params
+            if (isolate(input$bins_type_%widget_id%) == "num_of_bins") figure <- figure + 
+                ggplot2::geom_histogram(bins = isolate(input$num_of_bins_%widget_id%), fill = isolate(input$colour_%widget_id%), color = "#FFFFFF")
+            else if (isolate(input$bins_type_%widget_id%) == "bin_width") figure <- figure + 
+                ggplot2::geom_histogram(binwidth = isolate(input$bin_width_%widget_id%), fill = isolate(input$colour_%widget_id%), color = "#FFFFFF")
         }
         
+        # SCATTER PLOT
+        
+        if (isolate(input$plot_function_%widget_id%) == "geom_point"){
+            req(nrow(data$x) > 0, nrow(data$y) > 0)
+            
+            # If data is grouped by person_id
+            if (isolate(input$group_data_%widget_id%) & isolate(input$group_by_%widget_id% == "person_id")){
+                fig_data <- data$x %>%
+                    dplyr::transmute(person_id, variable = "x", value_as_number) %>%
+                    dplyr::bind_rows(
+                        data$y %>%
+                        dplyr::transmute(person_id, variable = "y", value_as_number)
+                    ) %>%
+                    dplyr::mutate(n = 1:dplyr::n()) %>%
+                    tidyr::pivot_wider(names_from = "variable", values_from = "value_as_number") %>%
+                    dplyr::select(-n) %>%
+                    dplyr::group_by(person_id) %>%
+                    dplyr::summarize(x = dplyr::first(na.omit(x)), y = dplyr::first(na.omit(y))) %>%
+                    dplyr::ungroup()
+            }
+            
+            # If data is not grouped or grouped by datetime
+            else fig_data <- data$x %>%
+                dplyr::rename(datetime = paste0(tolower(variable$x$domain_id), "_datetime")) %>%
+                dplyr::transmute(datetime, variable = "x", value_as_number) %>%
+                dplyr::bind_rows(
+                    data$y %>% 
+                    dplyr::rename(datetime = paste0(tolower(variable$y$domain_id), "_datetime")) %>%
+                    dplyr::transmute(datetime, variable = "y", value_as_number)
+                ) %>%
+                dplyr::mutate(n = 1:dplyr::n()) %>%
+                tidyr::pivot_wider(names_from = "variable", values_from = "value_as_number") %>%
+                dplyr::select(-n) %>%
+                dplyr::group_by(datetime) %>%
+                dplyr::summarize(x = dplyr::first(na.omit(x)), y = dplyr::first(na.omit(y))) %>%
+                dplyr::ungroup()
+            
+            # Create ggplot2 figure
+            figure <- fig_data %>% 
+                ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
+                ggplot2::geom_point(colour = isolate(input$colour_%widget_id%))
+        }
+        
+        # Add theme & labs
+        figure <- figure +
+            do.call(getFromNamespace(isolate(input$plot_theme_%widget_id%), "ggplot2"), list()) +
+            ggplot2::labs(x = isolate(input$x_label_%widget_id%), y = isolate(input$y_label_%widget_id%))
+        
+        # Final object of ggplot2 figure
         figure
     })
 })
 
 # Change SwatchColorPicker
+observeEvent(input$colour_pal_%widget_id%, {
+    %req%
 
-observeEvent(input$x_colour_pal_%widget_id%, {
-
-    pal <- RColorBrewer::brewer.pal(n = 8, name = input$x_colour_pal_%widget_id%)
+    pal <- RColorBrewer::brewer.pal(n = 8, name = input$colour_pal_%widget_id%)
     pal_tibble <- tibble::tibble(name = pal)
     colorCells <- list()
     for (i in 1:nrow(pal_tibble)) colorCells <- rlist::list.append(colorCells, list(id = pal_tibble[[i, "name"]], color = pal_tibble[[i, "name"]]))
     
-    output$x_colour_ui_%widget_id% <- renderUI(
-        shiny.fluent::SwatchColorPicker.shinyInput(ns("x_colour_%widget_id%"), colorCells = colorCells, columnCount = length(colorCells), value = pal[1])
-    )
+    # Get saved colour
+    value <- pal[1]
+    sql <- glue::glue_sql("SELECT * FROM aggregated_widgets_options WHERE widget_id = %widget_id% AND name = 'colour'", .con = r$db)
+    colour <- DBI::dbGetQuery(m$db, sql)
+    if (nrow(colour) > 0) if (colour %>% dplyr::pull(value) %in% pal) value <- colour %>% dplyr::pull(value)
+    
+    output$colour_ui_%widget_id% <- renderUI({
+        %req%
+        shiny.fluent::SwatchColorPicker.shinyInput(ns("colour_%widget_id%"), colorCells = colorCells, columnCount = length(colorCells), value = value)
+    })
 })
 
-observeEvent(input$y_colour_pal_%widget_id%, {
+# Save widget parameters
+observeEvent(input$save_%widget_id%, {
+    %req%
 
-    pal <- RColorBrewer::brewer.pal(n = 8, name = input$y_colour_pal_%widget_id%)
-    pal_tibble <- tibble::tibble(name = pal)
-    colorCells <- list()
-    for (i in 1:nrow(pal_tibble)) colorCells <- rlist::list.append(colorCells, list(id = pal_tibble[[i, "name"]], color = pal_tibble[[i, "name"]]))
+    # Delete old options
+    sql <- glue::glue_sql("DELETE FROM aggregated_widgets_options WHERE widget_id = {%widget_id%}", .con = m$db)
+    DBI::dbSendStatement(m$db, sql) -> query
+    DBI::dbClearResult(query)
     
-    output$y_colour_ui_%widget_id% <- renderUI(
-        shiny.fluent::SwatchColorPicker.shinyInput(ns("y_colour_%widget_id%"), colorCells = colorCells, columnCount = length(colorCells), value = pal[1])
-    )
+    last_row <- get_last_row(m$db, "aggregated_widgets_options")
+    
+    # Add new options
+    new_options <- tibble::tibble(
+        id = seq(last_row + 1, last_row + length(inputs)),
+        widget_id = %widget_id%, patient_id = NA_integer_, link_id = NA_integer_,
+        category = NA_character_, name = NA_character_, value = NA_character_, value_num = NA_real_,
+        creator_id = NA_integer_, datetime = as.character(Sys.time()), deleted = FALSE)
+    
+    new_options_values <- tibble::tibble(name = character(), value = character(), value_num = numeric())
+    
+    for (input_name in inputs){
+        if (input_name %in% spin_buttons){
+            value_num <- NA_real_
+            if (length(input[[paste0(input_name, "_%widget_id%")]]) > 0) value_num <- input[[paste0(input_name, "_%widget_id%")]]
+            new_options_values <- new_options_values %>% dplyr::bind_rows(tibble::tibble(name = input_name, value = NA_character_, value_num = value_num))
+        } 
+        else {
+            value <- NA_character_
+            if (length(input[[paste0(input_name, "_%widget_id%")]]) > 0) value <- as.character(input[[paste0(input_name, "_%widget_id%")]])
+            new_options_values <- new_options_values %>% dplyr::bind_rows(tibble::tibble(name = input_name, value = value, value_num = NA_real_))
+        } 
+    }
+    
+    for (col in c("name", "value", "value_num")) new_options[[col]] <- new_options_values[[col]]
+    
+    DBI::dbAppendTable(m$db, "aggregated_widgets_options", new_options)
+    
+    # Notify user
+    show_message_bar(output, "modif_saved", "success", i18n = i18n, ns = ns)
 })
--- plugins/aggregated/dbddcb34874bd3e8cfcc287f4dc73f422007d928e591add209329f03c99efa21/ui.R
+++ plugins/aggregated/dbddcb34874bd3e8cfcc287f4dc73f422007d928e591add209329f03c99efa21/ui.R
@@ -1,7 +1,38 @@
-x_variables <- convert_tibble_to_list(selected_concepts, key_col = "concept_id", text_col = "concept_name")
-y_variables <- convert_tibble_to_list(selected_concepts, key_col = "concept_id", text_col = "concept_name")
+concepts <- tibble::tibble(concept_id = 0L, concept_name = i18np$t("none")) %>% dplyr::bind_rows(selected_concepts %>% dplyr::select(concept_id, concept_name))
+x_variables <- convert_tibble_to_list(concepts, key_col = "concept_id", text_col = "concept_name")
+y_variables <- convert_tibble_to_list(concepts, key_col = "concept_id", text_col = "concept_name")
 
 palettes <- convert_tibble_to_list(data = tibble::tibble(pal = c("Set1", "Set2", "Set3", "Reds", "Purples", "Oranges", "Greens", "Blues", "Greys")), key_col = "pal", text_col = "pal")
+
+# List of inputs (to save & get saved params)
+
+dropdowns <- c("plot_function", "plot_theme", "stat", "bins_type", "x_variable", "y_variable", "colour_pal", "group_by", "summarize_fct")
+textfields <- c("x_label", "y_label")
+spin_buttons <- c("num_of_bins", "bin_width", "group_by_num")
+toggle_inputs <- "group_data"
+colour_inputs <- "colour"
+inputs <- c(dropdowns, textfields, spin_buttons, toggle_inputs, colour_inputs)
+
+inputs_values <- list()
+
+# Get saved params for this widget
+sql <- glue::glue_sql("SELECT * FROM aggregated_widgets_options WHERE widget_id = %widget_id%", .con = r$db)
+widget_options <- DBI::dbGetQuery(m$db, sql)
+if (nrow(widget_options) > 0){
+    for (input_name in inputs){
+        widget_option <- widget_options %>% dplyr::filter(name == input_name)
+        
+        if (nrow(widget_option) > 0){
+            if (input_name %in% spin_buttons) inputs_values[[input_name]] <- widget_option$value_num
+            else inputs_values[[input_name]] <- widget_option$value
+        }
+        else {
+            if (input_name %in% spin_buttons) inputs_values[[input_name]] <- ""
+            else inputs_values[[input_name]] <- NA_integer_
+        }
+    }
+}
+print(widget_options)
 
 splitLayout(
     cellWidths = c("50%", "50%"),
@@ -21,10 +52,10 @@ splitLayout(
                 condition = "input.current_tab_%widget_id% == 'plot_parameters_%widget_id%'", ns = ns, br(),
                 shiny.fluent::Dropdown.shinyInput(ns("plot_function_%widget_id%"), label = i18np$t("plot_choice"),
                     options = list(
-                        list(key = "geom_histogram", text = "geom_histogram"),
-                        list(key = "geom_bar", text = "geom_bar")
+                        list(key = "geom_histogram", text = paste0(i18np$t("histogram"), " (geom_histogram)")),
+                        list(key = "geom_point", text = paste0(i18np$t("scatter_plot"), " (geom_point)"))
                     ),
-                    value = "geom_histogram"),
+                    value = ifelse(inputs_values$plot_function == "", "geom_histogram", inputs_values$plot_function)),
                 shiny.fluent::Dropdown.shinyInput(ns("plot_theme_%widget_id%"), label = i18np$t("theme"),
                     options = list(
                         list(key = "theme_grey", text = "Grey"),
@@ -38,17 +69,17 @@ splitLayout(
                         list(key = "theme_void", text = "Void"),
                         list(key = "theme_test", text = "Test")
                     ),
-                    value = "theme_minimal"),
+                    value = ifelse(inputs_values$plot_theme == "", "theme_minimal", inputs_values$plot_theme)),
+                shiny.fluent::TextField.shinyInput(ns("x_label_%widget_id%"), label = i18np$t("x_label"), value = inputs_values$x_label),
+                shiny.fluent::TextField.shinyInput(ns("y_label_%widget_id%"), label = i18np$t("y_label"), value = inputs_values$y_label),
                 conditionalPanel(
                     condition = "input.plot_function_%widget_id% == 'geom_histogram'", ns = ns,
-                    shiny.fluent::TextField.shinyInput(ns("x_label_%widget_id%"), label = i18np$t("x_label")),
-                    shiny.fluent::TextField.shinyInput(ns("y_label_%widget_id%"), label = i18np$t("y_label")),
                     shiny.fluent::Dropdown.shinyInput(ns("stat_%widget_id%"), label = i18np$t("stat"),
                         options = list(
                             list(key = "bin", text = "bin"),
                             list(key = "count", text = "count")
                         ),
-                        value = "bin"),
+                        value = ifelse(inputs_values$stat == "", "bin", inputs_values$stat)),
                     shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
                         div(
                             shiny.fluent::Dropdown.shinyInput(ns("bins_type_%widget_id%"), label = i18np$t("bins"),
@@ -56,17 +87,19 @@ splitLayout(
                                     list(key = "num_of_bins", text = i18np$t("num_of_bins")),
                                     list(key = "bin_width", text = i18np$t("bin_width"))
                                 ),
-                                value = "num_of_bins"),
+                                value = ifelse(inputs_values$bins_type == "", "num_of_bins", inputs_values$bins_type)),
                             style = "width:50%"
                         ),
                         div(
                             conditionalPanel(
                                 condition = "input.bins_type_%widget_id% == 'num_of_bins'", ns = ns,
-                                shiny.fluent::SpinButton.shinyInput(ns("num_of_bins_%widget_id%"), label = i18np$t("bins_value"), value = 10, step = 5, min = 0, max = 2000)
+                                shiny.fluent::SpinButton.shinyInput(ns("num_of_bins_%widget_id%"), label = i18np$t("value"), 
+                                    value = ifelse(inputs_values$num_of_bins == NA_integer_, 10, inputs_values$num_of_bins), step = 5, min = 0, max = 2000)
                             ),
                             conditionalPanel(
                                 condition = "input.bins_type_%widget_id% == 'bin_width'", ns = ns,
-                                shiny.fluent::SpinButton.shinyInput(ns("bin_width_%widget_id%"), label = i18np$t("bins_value"), value = 1, step = 1, min = 0, max = 100)
+                                shiny.fluent::SpinButton.shinyInput(ns("bin_width_%widget_id%"), label = i18np$t("value"), 
+                                value = ifelse(inputs_values$bin_width == NA_integer_, 1, inputs_values$bin_width), step = 1, min = 0, max = 100)
                             ),
                             style = "width:50%; margin-top:28px;"
                         )
@@ -75,12 +108,69 @@ splitLayout(
             ),
             conditionalPanel(
                 condition = "input.current_tab_%widget_id% == 'variables_%widget_id%'", ns = ns, br(),
-                shiny.fluent::Dropdown.shinyInput(ns("x_variable_%widget_id%"), options = x_variables, label = i18np$t("x_variable")),
-                shiny.fluent::Dropdown.shinyInput(ns("x_colour_pal_%widget_id%"), options = palettes, value = "Set1", label = i18np$t("palette")),
-                uiOutput(ns("x_colour_ui_%widget_id%")),
-                shiny.fluent::Dropdown.shinyInput(ns("y_variable_%widget_id%"), options = y_variables, label = i18np$t("y_variable")),
-                shiny.fluent::Dropdown.shinyInput(ns("y_colour_pal_%widget_id%"), options = palettes, value = "Set1", label = i18np$t("palette")),
-                uiOutput(ns("y_colour_ui_%widget_id%")),
+                shiny.fluent::Dropdown.shinyInput(ns("x_variable_%widget_id%"), label = i18np$t("x_variable"),
+                    options = x_variables, value = ifelse(inputs_values$x_variable %in% c("0", ""), 0L, as.integer(inputs_values$x_variable))),
+                conditionalPanel(
+                    condition = "['geom_point'].includes(input.plot_function_%widget_id%)", ns = ns,
+                    shiny.fluent::Dropdown.shinyInput(ns("y_variable_%widget_id%"), label = i18np$t("y_variable"),
+                     options = y_variables, value = ifelse(inputs_values$y_variable %in% c("0", ""), 0L, as.integer(inputs_values$y_variable)))
+                ),
+                shiny.fluent::Dropdown.shinyInput(ns("colour_pal_%widget_id%"), options = palettes, 
+                    value = ifelse(inputs_values$colour_pal == "", "Set1", inputs_values$bins_type), label = i18np$t("palette")),
+                uiOutput(ns("colour_ui_%widget_id%")),
+                shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                    shiny.fluent::Toggle.shinyInput(ns("group_data_%widget_id%"), 
+                        value = ifelse(inputs_values$group_data == "", FALSE, as.logical(inputs_values$group_data)), style = "margin-top:5px;"),
+                    div(class = "toggle_title", i18np$t("group_data"), style = "padding-top:5px;")
+                ),
+                conditionalPanel(
+                    condition = "input.group_data_%widget_id% == true", ns = ns,
+                    shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                        div(
+                            shiny.fluent::Dropdown.shinyInput(ns("group_by_%widget_id%"), 
+                                options = list(
+                                    list(key = "person_id", text = i18np$t("person")),
+                                    list(key = "datetime", text = i18np$t("duration"))
+                                ), 
+                                value = ifelse(inputs_values$group_by == "", "datetime", inputs_values$group_by), label = i18np$t("group_by")),
+                            style = "width:33%"
+                        ),
+                        div(
+                            conditionalPanel(
+                                condition = "input.group_by_%widget_id% == 'datetime'", ns = ns,
+                                shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                                    div(
+                                        shiny.fluent::SpinButton.shinyInput(ns("group_by_num_%widget_id%"), label = "", 
+                                            value = ifelse(inputs_values$group_by_num == NA_integer_, 4, inputs_values$group_by_num), step = 1, min = 0, max = 2000), 
+                                        style = "width:50%; margin-top:29px;"
+                                    ),
+                                    div(
+                                        shiny.fluent::Dropdown.shinyInput(ns("group_by_type_%widget_id%"), 
+                                            options = list(
+                                                list(key = "minutes", text = i18np$t("minutes")),
+                                                list(key = "hours", text = i18np$t("hours")),
+                                                list(key = "days", text = i18np$t("days"))
+                                            ), 
+                                            value = ifelse(inputs_values$group_by_type == "", "hours", inputs_values$group_by_type), label = i18np$t("duration")),
+                                        style = "width:50%"
+                                    )
+                                )
+                            ),
+                            style = "width:67%"
+                        )
+                    ),
+                    div(
+                        shiny.fluent::Dropdown.shinyInput(ns("summarize_fct_%widget_id%"), 
+                            options = list(
+                                list(key = "min", text = i18np$t("min")),
+                                list(key = "max", text = i18np$t("max")),
+                                list(key = "mean", text = i18np$t("mean"))
+                            ), 
+                            value = ifelse(inputs_values$summarize_fct == "", "mean", inputs_values$summarize_fct), label = i18np$t("summarize_fct")
+                        ),
+                        style = "width:33%"
+                    )
+                )
             ),
         ),
         br(), br(),
@@ -92,4 +182,3 @@ splitLayout(
         )
     )
 )
-
