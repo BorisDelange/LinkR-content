4e686d5153ebac371ad0a906cebb8779588aa992
Boris Delange <boris.delange@gmail.com>
2023-08-12 12:14:04 +0200

    Add scripts plugin

--- plugins/aggregated/6f3c30ede116bc25978075b6634268214c545173634f3cd81c0d1db6081a45b8/ui.R
+++ plugins/aggregated/6f3c30ede116bc25978075b6634268214c545173634f3cd81c0d1db6081a45b8/ui.R
@@ -1,4 +1,26 @@
+# Get widget options
+sql <- glue::glue_sql("SELECT * FROM aggregated_widgets_options WHERE widget_id = %widget_id%", .con = r$db)
+widget_options <- DBI::dbGetQuery(m$db, sql)
+scripts <- widget_options %>% dplyr::filter(name == "script") %>% dplyr::select(id = value_num, name = value)
+selected_script <- NULL
+selected_script_type_choicegroup <- "r"
+selected_script_type <- "r"
+selected_script_code <- ""
+selected_script_result <- widget_options %>% dplyr::filter(name == "selected_script")
+if (nrow(selected_script_result) > 0) if ((selected_script_result %>% dplyr::pull(value_num)) %in% scripts$id){
+    selected_script <- selected_script_result %>% dplyr::pull(value_num)
+    script_options <- widget_options %>% dplyr::filter(name %in% c("script_type", "script_code") & link_id == selected_script)
+    
+    if (nrow(script_options) > 0){
+        selected_script_type_choicegroup <- script_options %>% dplyr::filter(name == "script_type") %>% dplyr::pull(value)
+        
+        if (selected_script_type_choicegroup == "rmarkdown") selected_script_type <- "markdown" else "r"
+        selected_script_code <- script_options %>% dplyr::filter(name == "script_code") %>% dplyr::pull(value)
+    }
+}
+
 tagList(
+    shiny.fluent::reactOutput(ns("delete_confirm_%widget_id%")),
     shiny.fluent::Pivot(
         id = ns("pivot_%widget_id%"),
         onLinkClick = htmlwidgets::JS(paste0("item => Shiny.setInputValue('", id, "-current_tab_%widget_id%', item.props.id)")),
@@ -8,7 +30,8 @@ tagList(
     conditionalPanel(
         condition = "input.current_tab_%widget_id% == 'script_%widget_id%' || input.current_tab_%widget_id% == null", ns = ns, br(),
         shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
-            div(shiny.fluent::Dropdown.shinyInput(ns("script_choice_%widget_id%")), style = "width:300px;"),
+            div(shiny.fluent::Dropdown.shinyInput(ns("script_choice_%widget_id%"), 
+                options = convert_tibble_to_list(scripts, key_col = "id", text_col = "name"), value = selected_script), style = "width:300px;"),
             div(style = "width:10px"),
             shiny.fluent::ChoiceGroup.shinyInput(
                 ns("script_type_%widget_id%"), className = "inline_choicegroup",
@@ -17,15 +40,16 @@ tagList(
                     list(key = "rmarkdown", text = i18np$t("rmarkdown")),
                     list(key = "plot", text = i18np$t("plot"))
                 ),
-                value = "r"
+                value = selected_script_type_choicegroup
             )
         ),
         div(shinyAce::aceEditor(
-            ns("script_code_%widget_id%"), "", mode = "r", 
+            ns("script_code_%widget_id%"), "", mode = selected_script_type, value = selected_script_code,
                 code_hotkeys = list(
                     "r", list(
                       run_selection = list(win = "CTRL-ENTER", mac = "CTRL-ENTER|CMD-ENTER"),
-                      run_all = list(win = "CTRL-SHIFT-ENTER", mac = "CTRL-SHIFT-ENTER|CMD-SHIFT-ENTER")
+                      run_all = list(win = "CTRL-SHIFT-ENTER", mac = "CTRL-SHIFT-ENTER|CMD-SHIFT-ENTER"),
+                      save = list(win = "CTRL-S", mac = "CTRL-S|CMD-S")
                     )
                 ),
                 autoScrollEditorIntoView = TRUE, minLines = 30, maxLines = 1000
@@ -54,7 +78,17 @@ tagList(
         )
     ),
     conditionalPanel(
-        condition = "input.current_tab_%widget_id% == 'scripts_management_%widget_id%' || input.current_tab_%widget_id% == null", ns = ns, br(),
-        DT::DTOutput(ns("scripts_management_datatable_%widget_id%"))
+        condition = "input.current_tab_%widget_id% == 'scripts_management_%widget_id%'", ns = ns, br(),
+        shiny.fluent::Stack(
+            horizontal = TRUE, tokens = list(childrenGap = 10),
+            make_textfield(i18n = i18n, ns = ns, label = "name", id = "script_name_%widget_id%", width = "300px"),
+            div(shiny.fluent::PrimaryButton.shinyInput(ns("add_script_%widget_id%"), i18n$t("add")), style = "margin-top:38px;")
+        ),
+        DT::DTOutput(ns("scripts_management_datatable_%widget_id%")),
+        shiny.fluent::Stack(
+            horizontal = TRUE, tokens = list(childrenGap = 10),
+            shiny.fluent::PrimaryButton.shinyInput(ns("save_scripts_%widget_id%"), i18n$t("save")),
+            shiny.fluent::DefaultButton.shinyInput(ns("delete_scripts_%widget_id%"), i18n$t("delete_selection"))
+        )
     )
 )
