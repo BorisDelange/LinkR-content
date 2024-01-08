0a28396ce6c4b0defa7145b0c3041b626ff704e2
Boris Delange <boris.delange@gmail.com>
2024-01-08 15:19:51 +0100

    Update plugins

---
+++ plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/server.R
@@ -0,0 +1,107 @@
+# Stats functions
+stats_functions <- list(
+    "mean" = function(x) mean(x, na.rm = TRUE) %>% round(1),
+    "iq1"  = function(x) quantile(x, probs = 0.25, na.rm = TRUE) %>% round(1),
+    "median"  = function(x) median(x, na.rm = TRUE) %>% round(1),
+    "iq3"  = function(x) quantile(x, probs = 0.75, na.rm = TRUE) %>% round(1),
+    "min"  = function(x) min(x, na.rm = TRUE) %>% round(1),
+    "max"  = function(x) max(x, na.rm = TRUE) %>% round(1)
+)
+
+# Stats functions per variable
+var_stats_functions <- list(
+    "age" = c("mean", "iq1", "median", "iq3", "min", "max"),
+    "gender" = "",
+    "mortality" = "",
+    "stays" = "",
+    "length_of_stay" = "",
+    "readmissions" = ""
+)
+
+# When a var is selected
+
+observeEvent(input$var_choice_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$var_choice_%widget_id%"))
+    
+    var_choice <- input$var_choice_%widget_id%
+    
+    # Requires that data is loaded
+    req(d$person %>% dplyr::count() %>% dplyr::pull() > 0)
+    req(m$omop_version)
+        
+    result_ui <- tagList()
+    result_plot <- ggplot2::ggplot()
+    result_table <- tibble::tibble(var = character(), value = numeric())
+    
+    data <- tibble::tibble()
+    
+    if (m$omop_version %in% c("5.3", "5.4")){
+        data <-
+            d$visit_detail %>%
+            dplyr::select(person_id, visit_detail_id, visit_detail_start_datetime, visit_detail_end_datetime) %>%
+            dplyr::left_join(
+                d$person %>%
+                    dplyr::mutate(birth_datetime = dplyr::case_when(
+                        is.na(birth_datetime) ~ lubridate::ymd_hms(paste0(year_of_birth, "-01-01 00:00:00")),
+                        TRUE ~ birth_datetime
+                    )) %>%
+                    dplyr::select(person_id, birth_datetime, gender_concept_id),
+                by = "person_id"
+            ) %>%
+            dplyr::relocate(gender_concept_id, .after = "person_id") %>%
+            dplyr::left_join(
+                d$death %>% dplyr::select(person_id, death_datetime),
+                by = "person_id"
+            ) %>%
+            dplyr::collect() %>%
+            dplyr::mutate(age = lubridate::interval(birth_datetime, visit_detail_start_datetime) / lubridate::years(1))
+    }
+    else if (m$omop_version == "6.0"){
+        data <-
+            d$visit_detail %>%
+            dplyr::select(person_id, visit_detail_id, visit_detail_start_datetime, visit_detail_end_datetime) %>%
+            dplyr::left_join(
+                d$person %>% dplyr::select(person_id, birth_datetime, death_datetime, gender_concept_id),
+                by = "person_id"
+            ) %>%
+            dplyr::relocate(gender_concept_id, .after = "person_id") %>%
+            dplyr::collect() %>%
+            dplyr::mutate(age = lubridate::interval(birth_datetime, visit_detail_start_datetime) / lubridate::years(1))
+    }
+    
+    # Age
+    if (var_choice == "age"){
+        
+        result_plot <- 
+            data %>%
+            dplyr::filter(!is.na(age)) %>%
+            ggplot2::ggplot(ggplot2::aes(x = age)) +
+            ggplot2::geom_histogram(bins = 50, fill = "#377EB8", color = "#FFFFFF") +
+            ggplot2::theme_minimal() +
+            ggplot2::labs(x = i18np$t("age"), y = i18np$t("stay_age_occurrences")) +
+            ggplot2::theme(
+                axis.title.x = ggplot2::element_text(color = "gray40"),
+                axis.title.y = ggplot2::element_text(color = "gray40"),
+                panel.grid.minor.x = ggplot2::element_blank()
+            ) +
+            ggplot2::scale_x_continuous(limits = c(-1, NA), breaks = seq(0.000, max(data$age, na.rm = TRUE), by = 10))
+            
+        for (stat_name in var_stats_functions[[var_choice]]){
+            stat_value <- stats_functions[[stat_name]](data$age)
+            result_table <- result_table %>% dplyr::bind_rows(
+                tibble::tibble(var = i18np$t(stat_name), value = stat_value)
+            )
+        }
+    }
+    
+    output$title_%widget_id% <- renderUI(
+        div(toupper(i18np$t(input$var_choice_%widget_id%)), style = paste0("background-color:#377EB8; color:white; font-weight:bold; font-size:18px; text-align:center; display:inline-block;",
+            "position:relative; right:50%; left:50%; transform: translate(-50%, -50%); margin-top:15px; padding:0px 5px 0px 5px;"))
+    )
+    
+    result_table <- setNames(result_table, c(i18np$t("variable"), i18np$t("value")))
+    
+    output$plot_%widget_id% <- renderPlot(result_plot)
+    output$table_%widget_id% <- renderTable(result_table)
+})
---
+++ plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/ui.R
@@ -0,0 +1,40 @@
+# Options for patients var dropdown
+var_choice_options <- list(
+    list(key = "age", text = i18np$t("age")),
+    list(key = "gender", text = i18np$t("gender")),
+    list(key = "mortality", text = i18np$t("mortality")),
+    list(key = "stays", text = i18np$t("stays")),
+    list(key = "length_of_stay", text = i18np$t("length_of_stay")),
+    list(key = "readmissions", text = i18np$t("readmissions"))
+)
+
+tagList(
+    #shiny.fluent::Pivot(
+    #    id = ns("pivot_%widget_id%"),
+    #    onLinkClick = htmlwidgets::JS(paste0("item => Shiny.setInputValue('", id, "-current_tab_%widget_id%', item.props.id)")),
+    #    shiny.fluent::PivotItem(id = "patients_tab_%widget_id%", itemKey = "plot", headerText = i18np$t("patients")),
+    #    shiny.fluent::PivotItem(id = "stays_tab_%widget_id%", itemKey = "code", headerText = i18np$t("stays"))
+    #),
+    div(
+        br(),
+        div(shiny.fluent::Dropdown.shinyInput(ns("var_choice_%widget_id%"), options = var_choice_options, value = "age"), style = "width:300px"),
+        div(
+            style = "display:flex;",
+            div(
+                id = ns("split_layout_left_%widget_id%"),
+                style = "padding-right:10px; width:50%;", br(),
+                uiOutput(ns("title_%widget_id%")),
+                div(plotOutput(ns("plot_%widget_id%")), style = "border:solid 1px;"),
+                div(tableOutput(ns("table_%widget_id%")), style = "border:solid 1px; margin-top:5px;")
+            ),
+            div(
+                id = ns("split_layout_right_%widget_id%"),
+                style = "padding-left:10px; width:50%;",
+                
+            )
+        )
+    ),
+    div(
+        id = ns("patients_tab_%widget_id%"),
+    )
+)
