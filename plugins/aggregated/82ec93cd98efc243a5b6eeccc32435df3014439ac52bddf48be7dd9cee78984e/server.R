b087cd36f33f02826217aed8592885388957fb29
Boris Delange <boris.delange@gmail.com>
2024-01-10 17:02:08 +0100

    Update plugins

--- plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/server.R
+++ plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/server.R
@@ -1,3 +1,6 @@
+# Config
+use_debounce <- TRUE
+
 # Stats functions
 stats_functions <- list(
     "mean" = function(x) mean(x, na.rm = TRUE) %>% round(1),
@@ -12,7 +15,7 @@ stats_functions <- list(
 
 # Stats functions per variable
 var_stats_functions <- list(
-    "age" = c("n_rows", "missing_data", "mean", "iq1", "median", "iq3", "min", "max"),
+    "age" = c("missing_data", "mean", "iq1", "median", "iq3", "min", "max"),
     "gender" = c("n_rows", "missing_data"),
     "mortality" = character(),
     "stays" = character(),
@@ -20,11 +23,143 @@ var_stats_functions <- list(
     "readmissions" = character()
 )
 
-# When a var is selected
+# -------------------------
+# --- Show / hide divs ----
+# -------------------------
+
+divs <- c("var_choice_%widget_id%", "split_layout_right_%widget_id%", "plot_size_div_%widget_id%")
+
+# Additional buttons for this plugin
+output$additional_buttons_%widget_id% <- renderUI(actionButton(ns("maximize_fig_%widget_id%"), "", icon = icon("maximize")))
+
+# Maximize fig
+observeEvent(input$maximize_fig_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$maximize_fig_%widget_id%"))
+    shinyjs::runjs(glue::glue("$('#{id}-split_layout_left_%widget_id%').css('width', '100%');"))
+    shinyjs::delay(100, {
+        output$additional_buttons_%widget_id% <- renderUI(actionButton(ns("minimize_fig_%widget_id%"), "", icon = icon("minimize")))
+        sapply(divs, shinyjs::hide)
+        m$create_plot_type_%widget_id% <- "show_plot"
+        m$create_plot_trigger_%widget_id% <- Sys.time()
+    })
+})
+
+# Minimize fig
+observeEvent(input$minimize_fig_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$minimize_fig_%widget_id%"))
+    shinyjs::runjs(glue::glue("$('#{id}-split_layout_left_%widget_id%').css('width', '50%');"))
+    shinyjs::delay(100, {
+        output$additional_buttons_%widget_id% <- renderUI(actionButton(ns("maximize_fig_%widget_id%"), "", icon = icon("maximize")))
+        sapply(divs, shinyjs::show)
+        m$create_plot_type_%widget_id% <- "show_plot"
+        m$create_plot_trigger_%widget_id% <- Sys.time()
+    })
+})
+
+# Show or hide stats table
+observeEvent(input$show_stats_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$show_stats_%widget_id%"))
+    
+    if (input$show_stats_%widget_id%) shinyjs::show("table_div_%widget_id%")
+    else shinyjs::hide("table_div_%widget_id%")
+})
+
+# Hide parameters div
+observeEvent(input$show_params_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$show_params_%widget_id%"))
+
+    if (input$show_params_%widget_id%){
+        shinyjs::runjs(glue::glue("$('#{id}-split_layout_left_%widget_id%').css('width', '50%');"))
+        shinyjs::show("split_layout_right_%widget_id%")
+    }
+    else {
+        shinyjs::hide("split_layout_right_%widget_id%")
+        shinyjs::runjs(glue::glue("$('#{id}-split_layout_left_%widget_id%').css('width', '100%');"))
+    }
+    
+    # Reload plot
+    m$create_plot_type_%widget_id% <- "show_plot"
+    m$create_plot_trigger_%widget_id% <- Sys.time()
+})
+
+# Table & plot side by side
+observeEvent(input$side_by_side_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$side_by_side_%widget_id%"))
+    
+    if (input$side_by_side_%widget_id%){
+        shinyjs::runjs(glue::glue("$('#{id}-table_and_plot_div_%widget_id%').css('display', 'flex');"))
+        shinyjs::runjs(glue::glue("$('#{id}-plot_div_%widget_id%').css('width', '70%');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('width', '30%');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('margin-top', '0');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('margin-left', '5px');"))
+    }
+    else {
+        shinyjs::runjs(glue::glue("$('#{id}-table_and_plot_div_%widget_id%').css('display', 'block');"))
+        shinyjs::runjs(glue::glue("$('#{id}-plot_div_%widget_id%').css('width', '100%');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('width', '100%');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('margin-top', '5px');"))
+        shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('margin-left', '0');"))
+    }
+    m$render_results_%widget_id% <- Sys.time()  
+})
+
+# Table & plot width & height
+
+# sapply(c("width", "height"), function(name){
+#     
+#     if (use_debounce) observeEvent(input[[paste0("plot_", name, "_%widget_id%")]], {
+#         %req%
+#         if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$plot_", name, "_%widget_id%"))
+#         
+#         if (name == "width") value <- paste0(input$plot_width_%widget_id%, "%")
+#         else value <- input$plot_height_%widget_id% * 0.01 * 400
+#         
+#         shinyjs::runjs(glue::glue("$('#{id}-plot_div_%widget_id%').css('{name}', '{value}');"))
+#         m$render_results_%widget_id% <- Sys.time()
+#     }) %>% debounce(1000)
+#     
+#     else {
+#         m[[paste0("debounce_plot_", name, "_%widget_id%")]] <- Sys.time()
+#         observeEvent(input[[paste0("plot_", name, "_%widget_id%")]], {
+#             %req%
+#             if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$plot_", name, "_%widget_id%"))
+#             
+#             if (name == "width") value <- paste0(input$plot_width_%widget_id%, "%")
+#             else value <- input$plot_height_%widget_id% * 0.01 * 400
+#             
+#             # Manual debounce (it seems debounce doesn't work everywhere)
+#             
+#             if (difftime(Sys.time(), m[[paste0("debounce_plot_", name, "_%widget_id%")]], units = "secs") > 1){
+#                 m[[paste0("debounce_plot_", name, "_%widget_id%")]] <- Sys.time()
+#                 
+#                 shinyjs::runjs(glue::glue("$('#{id}-plot_div_%widget_id%').css('{name}', '{value}');"))
+#                 if (name == "width") shinyjs::runjs(glue::glue("$('#{id}-table_div_%widget_id%').css('width', '{100 - input$plot_width_%widget_id%}%');"))
+#                 
+#                 m$create_plot_type_%widget_id% <- "show_plot"
+#                 m$create_plot_trigger_%widget_id% <- Sys.time()
+#             }
+#         })
+#     }
+# })
+
+# ----------------------------
+# --- Render plot & table ----
+# ----------------------------
 
 observeEvent(input$var_choice_%widget_id%, {
     %req%
     if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer input$var_choice_%widget_id%"))
+    m$render_results_%widget_id% <- Sys.time()  
+})
+
+observeEvent(m$render_results_%widget_id%, {
+    %req%
+    if (debug) cat(paste0("\n", Sys.time(), " - mod_", id, " - widget_id = %widget_id% - observer r$render_results_%widget_id%"))
     
     var_choice <- input$var_choice_%widget_id%
     
@@ -91,7 +226,7 @@ observeEvent(input$var_choice_%widget_id%, {
                 axis.title.y = ggplot2::element_text(color = "gray40"),
                 panel.grid.minor.x = ggplot2::element_blank()
             ) +
-            ggplot2::scale_x_continuous(limits = c(-1, NA), breaks = seq(0.000, max(data$age, na.rm = TRUE), by = 10))
+            ggplot2::scale_x_continuous(limits = c(-1, NA), breaks = seq(0.0, max(data$age, na.rm = TRUE), by = 10))
             
         # Apply stats functions
         for (stat_name in var_stats_functions[[var_choice]]){
@@ -101,14 +236,27 @@ observeEvent(input$var_choice_%widget_id%, {
             )
         }
         
+        n_unique_patients <- data %>% dplyr::summarise(n = dplyr::n_distinct(person_id)) %>% dplyr::pull()
+        
         result_table <-
             result_table %>%
+            dplyr::mutate(n = 1:dplyr::n()) %>%
+            dplyr::mutate(n = dplyr::case_when(n >= 2 ~ n + 2, TRUE ~ n)) %>%
+            dplyr::bind_rows(
+                tibble::tribble(
+                    ~var, ~value, ~n,
+                    i18np$t("n_stays"), length(data$age), 1,
+                    i18np$t("n_unique_patients"), n_unique_patients, 2,
+                )
+            ) %>%
             dplyr::mutate(
                 value = dplyr::case_when(
                     var %in% c(i18np$t("missing_data"), i18np$t("n_rows")) ~ as.character(as.integer(value)),
                     TRUE ~ as.character(value)
                 )
-            )
+            ) %>%
+            dplyr::arrange(n) %>%
+            dplyr::select(-n)
     }
     
     # Gender
--- plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/ui.R
+++ plugins/aggregated/82ec93cd98efc243a5b6eeccc32435df3014439ac52bddf48be7dd9cee78984e/ui.R
@@ -18,13 +18,47 @@ tagList(
                 id = ns("split_layout_left_%widget_id%"),
                 style = "padding-right:10px; width:50%;", br(),
                 uiOutput(ns("title_%widget_id%")),
-                div(plotOutput(ns("plot_%widget_id%")), style = "border:solid 2px #EFEEEE;"),
-                div(tableOutput(ns("table_%widget_id%")), style = "border:solid 2px #EFEEEE; margin-top:5px;")
+                div(
+                    id = ns("table_and_plot_div_%widget_id%"),
+                    div(id = ns("plot_div_%widget_id%"), plotOutput(ns("plot_%widget_id%")), style = "border:solid 2px #EFEEEE; width:70%;"),
+                    div(id = ns("table_div_%widget_id%"), tableOutput(ns("table_%widget_id%")), style = "border:solid 2px #EFEEEE; margin-top:5px; width:30%;"),
+                    style = "display:flex;"
+                ),
+                br(),
+                div(
+                    id = ns("plot_size_div_%widget_id%"),
+                    shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                        div(
+                            div(strong(i18np$t("plot_width")), style = "margin-top:6px;"),
+                            div(shiny.fluent::Slider.shinyInput(ns("plot_width_%widget_id%"), value = 100, min = 1, max = 100), style = "width:200px; margin-left:-8px; padding-top:4px;"),
+                            style = "width:33%; max-width:200px;"
+                        ),
+                        div(
+                            div(strong(i18np$t("plot_height")), style = "margin-top:6px;"),
+                            div(shiny.fluent::Slider.shinyInput(ns("plot_height_%widget_id%"), value = 100, min = 1, max = 100), style = "width:200px; margin-left:-8px; padding-top:4px;"),
+                            style = "width:33%; max-width:200px;"
+                        ),
+                        div(
+                            shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                                shiny.fluent::Toggle.shinyInput(ns("show_params_%widget_id%"), value = TRUE, style = "margin-top:5px;"),
+                                div(class = "toggle_title", i18np$t("show_params"), style = "padding-top:5px;")
+                            ),
+                            style = "width:34%; max-width:300px; margin-top:15px;"
+                        )
+                    )
+                )
             ),
             div(
                 id = ns("split_layout_right_%widget_id%"),
-                style = "padding-left:10px; width:50%;",
-                
+                style = "padding-left:10px; width:50%; margin-top:60px;",
+                shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                    shiny.fluent::Toggle.shinyInput(ns("show_stats_%widget_id%"), value = TRUE, style = "margin-top:5px;"),
+                    div(class = "toggle_title", i18np$t("show_stats"), style = "padding-top:5px;")
+                ),
+                shiny.fluent::Stack(horizontal = TRUE, tokens = list(childrenGap = 10),
+                    shiny.fluent::Toggle.shinyInput(ns("side_by_side_%widget_id%"), value = TRUE, style = "margin-top:5px;"),
+                    div(class = "toggle_title", i18np$t("table_plot_side_by_side"), style = "padding-top:5px;")
+                )
             )
         )
     ),
