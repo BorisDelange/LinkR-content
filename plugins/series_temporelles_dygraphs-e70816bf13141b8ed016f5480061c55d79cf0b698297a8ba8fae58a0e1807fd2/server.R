00040ae074b4fa345a795cd4b47cffe86b3e9e6d
username <email@email.com>
2024-06-26 17:44:09 +0200

    Add dygraphs plugin (linkr v0.3)

---
+++ plugins/series_temporelles_dygraphs-e70816bf13141b8ed016f5480061c55d79cf0b698297a8ba8fae58a0e1807fd2/server.R
@@ -0,0 +1,72 @@
+# -------
+# Tabs --
+# -------
+
+## All tabs
+tabs <- c("figure", "figure_settings", "code", "general_settings")
+
+## Create an observer by tab, show selected tab, hide all others
+sapply(tabs, function(tab){
+
+    observeEvent(input[[paste0(tab, "_%widget_id%")]], {
+        %req%
+        
+        if (tab == "general_settings") shinyjs::hide("figure_settings_code_div_%widget_id%")
+        else shinyjs::show("figure_settings_code_div_%widget_id%")
+        
+        sapply(paste0(setdiff(tabs, tab), "_div_%widget_id%"), shinyjs::hide)
+        shinyjs::show(paste0(tab, "_div_%widget_id%"))
+        
+        if (tab %in% c("figure_settings", "code")){
+            if (length(input$figure_and_settings_side_by_side_%widget_id%) > 0) if (input$figure_and_settings_side_by_side_%widget_id%) shinyjs::show("figure_div_%widget_id%")
+            shinyjs::show("figure_settings_code_sidenav_%widget_id%")
+        }
+        else shinyjs::hide("figure_settings_code_sidenav_%widget_id%")
+
+    })
+})
+
+# ---------
+# Figure --
+# ---------
+
+%import_script('server_figure.R')%
+
+# ------------------
+# Figure settings --
+# ------------------
+
+
+
+# -------
+# Code --
+# -------
+
+
+
+# -----------
+# Settings --
+# -----------
+
+# Settings / editor side-by-side with figure
+
+observeEvent(input$figure_and_settings_side_by_side_%widget_id%, {
+    %req%
+    
+    if (input$figure_and_settings_side_by_side_%widget_id%){
+        shinyjs::runjs(paste0(
+            "$('#", id, "-figure_div_%widget_id%').css('width', '50%');",
+            "$('#", id, "-figure_settings_div_%widget_id%').css('width', '50%');",
+            "$('#", id, "-code_div_%widget_id%').css('width', '50%');"
+        ))
+        shinyjs::hide("figure_%widget_id%")
+    }
+    else {
+        shinyjs::runjs(paste0(
+            "$('#", id, "-figure_div_%widget_id%').css('width', '100%');",
+            "$('#", id, "-figure_settings_div_%widget_id%').css('width', '100%');",
+            "$('#", id, "-code_div_%widget_id%').css('width', '100%');"
+        ))
+        shinyjs::show("figure_%widget_id%")
+    }
+})
