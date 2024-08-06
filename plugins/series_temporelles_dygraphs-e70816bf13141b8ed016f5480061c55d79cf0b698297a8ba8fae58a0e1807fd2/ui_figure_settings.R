5d9d1f932cc616abdb3009821c07c1f2d9920eca
Boris Delange <boris.delange@proton.me>
2024-08-06 16:27:58 +0200

    Update README from LinkR

--- plugins/series_temporelles_dygraphs-e70816bf13141b8ed016f5480061c55d79cf0b698297a8ba8fae58a0e1807fd2/ui_figure_settings.R
+++
@@ -1,7 +0,0 @@
-div(
-    div(
-        shiny.fluent::Dropdown.shinyInput(ns("variable_%widget_id%"), options = convert_tibble_to_list(selected_concepts, key_col = "concept_id", text_col = "concept_name")),
-        style = "width: 200px;"
-    ),
-    style = "height: calc(100% - 10px); padding: 5px;"  
-)
