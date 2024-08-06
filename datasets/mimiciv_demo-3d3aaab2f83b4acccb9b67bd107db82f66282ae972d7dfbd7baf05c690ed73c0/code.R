6e3edaef55867c952d87d936dd57ed96eb796144
Boris Delange <boris.delange@proton.me>
2024-08-06 16:28:40 +0200

    Update README from LinkR

--- datasets/mimiciv_demo-3d3aaab2f83b4acccb9b67bd107db82f66282ae972d7dfbd7baf05c690ed73c0/code.R
+++
@@ -1,5 +0,0 @@
-import_dataset(
-    r, d, dataset_id = %dataset_id%, omop_version = "5.3",
-    data_source = "disk", data_folder = "/Users/borisdelange/Documents/Datasets/mimic-iv-demo",
-    read_with = "duckdb", save_as = "parquet", rewrite = FALSE
-)
