0e43255d82dd7712442a1e977a48b529340e22bd
Jane Doe <boris.delange@proton.me>
2024-09-08 22:19:28 +0200

    Update dataset from LinkR

--- datasets/mimiciv_demo_set-44b60e04eae66bcd8fc86d3196c09db1620ceec65d759de6080cda702aa45b10/code.R
+++ datasets/mimiciv_demo_set-44b60e04eae66bcd8fc86d3196c09db1620ceec65d759de6080cda702aa45b10/code.R
@@ -46,6 +46,5 @@ for (table in tables) {
 # Import data
 import_dataset(
     r, d, dataset_id = %dataset_id%, omop_version = "5.3",
-    data_source = "disk", data_folder = "%dataset_folder%",
-    read_with = "duckdb", save_as = "parquet", rewrite = FALSE
+    data_source = "disk", data_folder = "%dataset_folder%"
 )
