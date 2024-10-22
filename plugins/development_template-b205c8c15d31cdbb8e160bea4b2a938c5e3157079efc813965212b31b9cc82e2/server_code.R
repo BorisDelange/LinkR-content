4f5ba7984c4d88305b7f74a09e96abc2f4be1db9
Jane Doe <boris.delange@proton.me>
2024-10-22 22:43:56 +0200

    Fix bug with console user access (#15)

--- plugins/development_template-b205c8c15d31cdbb8e160bea4b2a938c5e3157079efc813965212b31b9cc82e2/server_code.R
+++ plugins/development_template-b205c8c15d31cdbb8e160bea4b2a938c5e3157079efc813965212b31b9cc82e2/server_code.R
@@ -48,8 +48,6 @@ observeEvent(input$code_%widget_id%_run_all, {
     %req%
     if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$code_run_all"))
 
-    req("projects_console_access" %in% user_accesses)
-
     m$code_%widget_id% <- input$code_%widget_id%
     shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
 })
@@ -58,8 +56,6 @@ observeEvent(input$code_%widget_id%_run_all, {
 observeEvent(input$display_figure_%widget_id%, {
     %req%
     if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$display_figure"))
-    
-    req("projects_console_access" %in% user_accesses)
     
     m$code_%widget_id% <- input$code_%widget_id%
     shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
