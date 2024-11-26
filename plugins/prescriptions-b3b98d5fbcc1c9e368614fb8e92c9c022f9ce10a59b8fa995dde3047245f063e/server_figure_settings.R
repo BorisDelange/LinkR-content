cd870f2d825f47f832f8570c79fe7819ae2c9c5f
Jane Doe <boris.delange@proton.me>
2024-11-26 08:07:17 +0100

    Fix bug on timelines synchronization (#7)

--- plugins/prescriptions-b3b98d5fbcc1c9e368614fb8e92c9c022f9ce10a59b8fa995dde3047245f063e/server_figure_settings.R
+++ plugins/prescriptions-b3b98d5fbcc1c9e368614fb8e92c9c022f9ce10a59b8fa995dde3047245f063e/server_figure_settings.R
@@ -114,6 +114,7 @@ observeEvent(m$debounced_datetimes_timeline_%tab_id%(), {
     %req%
     req(input$synchronize_timelines_%widget_id%)
     req(length(m$debounced_datetimes_timeline_%tab_id%()) > 0)
+    req(length(m$datetimes_%widget_id%) > 0)
     if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer m$debounced_datetimes_timeline"))
     
     tryCatch({
