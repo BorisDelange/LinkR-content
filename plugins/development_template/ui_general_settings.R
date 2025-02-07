378d1329662881eeaf35e9ed75e149d5abc62505
Jane Doe <boris.delange@proton.me>
2025-02-07 18:44:14 +0100

    Restrict access to console (#15)

--- plugins/development_template/ui_general_settings.R
+++ plugins/development_template/ui_general_settings.R
@@ -23,25 +23,25 @@ div(
             style = "display: flex; margin-top: 5px;" 
         )
     ),
-#     div(
-#         tags$strong(i18np$t("code_execution")), br(),
-#         div(
-#             shiny.fluent::Toggle.shinyInput(
-#                 ns("run_code_at_settings_file_load_%widget_id%"), value = toggle_values$run_code_at_settings_file_load,
-#                 onClick = htmlwidgets::JS(paste0("item => {Shiny.setInputValue('", id, "-save_general_settings_%widget_id%', Math.random());}"))
-#             ),
-#             tags$label(i18np$t("run_code_at_settings_file_load"), `for` = ns("run_code_at_settings_file_load_%widget_id%"), style = "margin-left: 5px;"),
-#             style = "display: flex; margin-top: 8px;" 
-#         ),
-#         div(
-#             shiny.fluent::Toggle.shinyInput(
-#                 ns("run_code_on_data_update_%widget_id%"), value = toggle_values$run_code_on_data_update,
-#                 onClick = htmlwidgets::JS(paste0("item => {Shiny.setInputValue('", id, "-save_general_settings_%widget_id%', Math.random());}"))
-#             ),
-#             tags$label(i18np$t("run_code_on_data_update"), `for` = ns("run_code_on_data_update_%widget_id%"), style = "margin-left: 5px;"),
-#             style = "display: flex; margin-top: 5px;" 
-#         ),
-#         style = "margin-top: 10px;"
-#     ),
+    # div(
+        # tags$strong(i18np$t("code_execution")), br(),
+        # div(
+            # shiny.fluent::Toggle.shinyInput(
+                # ns("run_code_at_settings_file_load_%widget_id%"), value = toggle_values$run_code_at_settings_file_load,
+                # onClick = htmlwidgets::JS(paste0("item => {Shiny.setInputValue('", id, "-save_general_settings_%widget_id%', Math.random());}"))
+            # ),
+            # tags$label(i18np$t("run_code_at_settings_file_load"), `for` = ns("run_code_at_settings_file_load_%widget_id%"), style = "margin-left: 5px;"),
+            # style = "display: flex; margin-top: 8px;" 
+        # ),
+        # div(
+            # shiny.fluent::Toggle.shinyInput(
+                # ns("run_code_on_data_update_%widget_id%"), value = toggle_values$run_code_on_data_update,
+                # onClick = htmlwidgets::JS(paste0("item => {Shiny.setInputValue('", id, "-save_general_settings_%widget_id%', Math.random());}"))
+            # ),
+            # tags$label(i18np$t("run_code_on_data_update"), `for` = ns("run_code_on_data_update_%widget_id%"), style = "margin-left: 5px;"),
+            # style = "display: flex; margin-top: 5px;" 
+        # ),
+        # style = "margin-top: 10px;"
+    # ),
     style = "padding: 10px;"
 )
