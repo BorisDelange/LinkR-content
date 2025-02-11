974757c437d125e0541009ef55a917d44c449e29
Jane Doe <boris.delange@proton.me>
2025-02-11 08:31:55 +0100

    Update plugin from LinkR

--- plugins/timeline_dygraphs/server_figure_settings.R
+++ plugins/timeline_dygraphs/server_figure_settings.R
@@ -18,13 +18,11 @@ observeEvent(input$load_figure_settings_%widget_id%, {
             value <- figure_settings %>% dplyr::filter(name == !!name) %>% dplyr::pull(value)
             value_num <- figure_settings %>% dplyr::filter(name == !!name) %>% dplyr::pull(value_num)
             
-            if (name %in% c("data_source", "concepts_choice", "aggregate_fct")) shiny.fluent::updateDropdown.shinyInput(session, paste0(name, "_%widget_id%"), value = value)
-            else if (name %in% c("concepts", "concept_classes")){
-                value <- unlist(strsplit(value, ", "))
-                if (name == "concepts") value <- as.numeric(value)
+            if (name == "data_source") shiny.fluent::updateDropdown.shinyInput(session, paste0(name, "_%widget_id%"), value = value)
+            else if (name == "concepts"){
+                value <- as.numeric(unlist(strsplit(value, ", ")))
                 shiny.fluent::updateDropdown.shinyInput(session, paste0(name, "_%widget_id%"), value = value)
             }
-            else if (name == "num_cols") shiny.fluent::updateSpinButton.shinyInput(session, paste0(name, "_%widget_id%"), value = value_num)
             else if (name == "synchronize_timelines"){
                 value <- as.logical(value_num)
                 shiny.fluent::updateToggle.shinyInput(session, paste0(name, "_%widget_id%"), value = value)
@@ -33,11 +31,8 @@ observeEvent(input$load_figure_settings_%widget_id%, {
     }
     
     # Run code if toggle is activated
-    if (length(input$run_code_at_settings_file_load_%widget_id%) > 0){
-        if (input$run_code_at_settings_file_load_%widget_id%){
-           shinyjs::delay(500, shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-display_figure_%widget_id%', Math.random());")))
-       }
-    }
+    if (length(input$run_code_at_settings_file_load_%widget_id%) > 0) if (input$run_code_at_settings_file_load_%widget_id%) shinyjs::delay(500, 
+        shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-display_figure_%widget_id%', Math.random());")))
 })
 
 # Save current settings
@@ -62,11 +57,7 @@ observeEvent(input$save_params_and_code_%widget_id%, {
             new_data <- tibble::tribble(
                 ~name, ~value, ~value_num,
                 "data_source", input$data_source_%widget_id%, NA_real_,
-                "concepts_choice", input$concepts_choice_%widget_id%, NA_real_,
-                "concept_classes", input$concept_classes_%widget_id% %>% toString(), NA_real_,
                 "concepts", input$concepts_%widget_id% %>% toString(), NA_real_,
-                "num_cols", NA_character_, input$num_cols_%widget_id%,
-                "aggregate_fct", input$aggregate_fct_%widget_id%, NA_real_,
                 "synchronize_timelines", NA_character_, as.integer(input$synchronize_timelines_%widget_id%)
             )
             
@@ -86,24 +77,6 @@ observeEvent(input$save_params_and_code_%widget_id%, {
     }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
 })
 
-# Show / hide concepts_div and concept_classes_div
-
-observeEvent(input$concepts_choice_%widget_id%, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$concepts_choice_"))
-    
-   
-    if (input$concepts_choice_%widget_id% == "selected_concepts"){
-        shinyjs::hide("concept_classes_div_%widget_id%")
-        shinyjs::show("concepts_div_%widget_id%")
-    }
-    else if (input$concepts_choice_%widget_id% == "selected_concept_classes"){
-        shinyjs::hide("concepts_div_%widget_id%")
-        shinyjs::show("concept_classes_div_%widget_id%")
-    }
-    else sapply(c("concepts_div_%widget_id%", "concept_classes_div_%widget_id%"), shinyjs::hide)
-})
-
 # Synchronize timelines
 
 # Create a reactiveVal with timeline datetimes
@@ -112,29 +85,35 @@ if (length(m$datetimes_timeline_%tab_id%) == 0){
     m$debounced_datetimes_timeline_%tab_id% <- reactive(m$datetimes_timeline_%tab_id%()) %>% debounce(500)
 }
 
-m$debounced_datetime_slider_%widget_id% <- reactive(input$datetime_slider_%widget_id%) %>% debounce(500)
-
-observeEvent(m$debounced_datetime_slider_%widget_id%(), {
+observeEvent(input$synchronize_timelines_%widget_id%, {
     %req%
-    req(length(m$debounced_datetime_slider_%widget_id%()) > 0)
-    req(length(m$datetimes_%widget_id%) > 0)
-    req(length(m$data_datetimes_range_%widget_id%) > 0)
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer m$debounced_datetime_slider"))
+    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$synchronize_timelines"))
     
     tryCatch({
-        if (
-            m$debounced_datetime_slider_%widget_id%()[[1]] >= m$data_datetimes_range_%widget_id%[[1]] &
-            m$debounced_datetime_slider_%widget_id%()[[2]] <= m$data_datetimes_range_%widget_id%[[2]] 
-        ){
-            m$datetimes_timeline_%tab_id%(m$debounced_datetime_slider_%widget_id%() %>% as.POSIXct())
-            
-            if (
-                isFALSE(input$synchronize_timelines_%widget_id%) &
-                (abs(as.numeric(m$debounced_datetime_slider_%widget_id%()[[1]]) - as.numeric(m$datetimes_%widget_id%[[1]])) > 5 |
-                abs(as.numeric(m$debounced_datetime_slider_%widget_id%()[[2]]) - as.numeric (m$datetimes_%widget_id%[[2]])) > 5)){
-                shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
-            }
+        # Add a padding to align widget's timeline with other widgets
+        if (input$synchronize_timelines_%widget_id%) {
+            shinyjs::runjs(sprintf(
+                "document.getElementById('%s').style.paddingLeft = '80px'; var event = new Event('resize'); window.dispatchEvent(event);",
+                ns("dygraph_div_%widget_id%")
+            ))
+        } else {
+            shinyjs::runjs(sprintf(
+                "document.getElementById('%s').style.paddingLeft = '0px'; var event = new Event('resize'); window.dispatchEvent(event);",
+                ns("dygraph_div_%widget_id%")
+            ))
         }
+    }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
+})
+
+observeEvent(input$dygraph_%widget_id%_date_window, {
+    %req%
+    req(input$synchronize_timelines_%widget_id%)
+    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$dygraph_%widget_id%_date_window"))
+        
+    tryCatch({
+        datetime_values <- as.POSIXct(input$dygraph_%widget_id%_date_window, format = "%Y-%m-%dT%H:%M:%OSZ", tz = "UTC")
+        m$datetimes_timeline_%tab_id%(datetime_values)
+        
     }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
 })
 
@@ -146,7 +125,7 @@ observeEvent(m$debounced_datetimes_timeline_%tab_id%(), {
     if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer m$debounced_datetimes_timeline"))
     
     tryCatch({
-        
+    
         if (
             abs(as.numeric(m$debounced_datetimes_timeline_%tab_id%()[[1]]) - as.numeric(m$datetimes_%widget_id%[[1]])) > 5 |
             abs(as.numeric(m$debounced_datetimes_timeline_%tab_id%()[[2]]) - as.numeric (m$datetimes_%widget_id%[[2]])) > 5){
