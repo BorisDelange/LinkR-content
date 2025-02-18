5dd69e2299c99ea2fbf834f1ba5bf09fc7a2b3b0
Jane Doe <boris.delange@proton.me>
2025-02-18 12:35:40 +0100

    Update project from LinkR

---
+++ projects/linkr_demo/widgets/aggregated_data/overview/number_of_patients/number_of_patients.R
@@ -0,0 +1,33 @@
+patients <- d$data_subset$person %>% dplyr::collect()
+
+if (m$language == "en") legend <- "patients" else if (m$language == "fr") legend <- "patients"
+
+res <- div()
+
+if (nrow(patients) > 0) {
+    res <- 
+    div(
+        style = "
+            text-align: center; 
+            height: 100%; 
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+        ",
+        div(
+            style = "color: #428BCA; margin-bottom: 10px;",
+            tags$i(class = "fas fa-hospital-user fa-2x")
+        ),
+        div(
+            style = "color: #428BCA; font-size: 36px; font-weight: bold; margin: 10px 0;",
+            nrow(patients)
+        ),
+        div(
+            style = "color: #666; font-size: 14px; text-transform: uppercase;",
+            legend
+        )
+    )
+}
+
+res
