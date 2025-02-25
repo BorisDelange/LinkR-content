bb398677d3aa20c9e05ae063d1789c7b84f24b94
Jane Doe <boris.delange@proton.me>
2025-02-25 08:43:27 +0100

    Fix bug with settings files

--- plugins/timeline_dygraphs/server_settings_files.R
+++ plugins/timeline_dygraphs/server_settings_files.R
@@ -102,21 +102,6 @@ observeEvent(input$add_settings_file_%widget_id%, {
     }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
 })
 
-## Update dropdown
-# observeEvent(input$reload_dropdown_%widget_id%, {
-    # %req%
-    # if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$settings_file"))
-    
-    # tryCatch({
-        # sql <- glue::glue_sql("SELECT id, value AS name FROM widgets_options WHERE widget_id = %widget_id% AND category = 'settings_files' AND name = 'file_name'", .con = m$db)
-        # m$settings_filenames_%widget_id% <- DBI::dbGetQuery(m$db, sql)
-        
-        # dropdown_options <- convert_tibble_to_list(m$settings_filenames_%widget_id%, key_col = "id", text_col = "name")
-        # shiny.fluent::updateDropdown.shinyInput(session, "settings_file_%widget_id%", options = dropdown_options)
-        
-    # }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
-# })
-
 ## A settings file is selected
 observeEvent(input$settings_file_%widget_id%, {
     %req%
@@ -174,6 +159,9 @@ observeEvent(input$confirm_file_deletion_%widget_id%, {
         
         # Delete row in db
         sql_send_statement(m$db, glue::glue_sql("DELETE FROM widgets_options WHERE id = {file_id}", .con = m$db))
+        
+        # Delete selected file in widgets_options
+        sql_send_statement(m$db, glue::glue_sql("DELETE FROM widgets_options WHERE widget_id = %widget_id% AND category = 'general_settings' AND name = 'selected_file_id'", .con = m$db))
         
         # Update filenames var
         m$settings_filenames_%widget_id% <- m$settings_filenames_%widget_id% %>% dplyr::filter(id != file_id)
