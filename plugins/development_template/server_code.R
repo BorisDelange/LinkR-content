ca8cee50457636f4fde20fa48c8aab6bdb2ca298
Boris Delange <boris.delange@proton.me>
2025-05-28 21:41:55 +0000

    Update 7 files
    - /plugins/development_template/translations.csv
    - /plugins/development_template/server.R
    - /plugins/development_template/server_settings_files.R
    - /plugins/development_template/server_general_settings.R
    - /plugins/development_template/server_figure_settings.R
    - /plugins/development_template/server_code.R
    - /plugins/development_template/plugin.xml

--- plugins/development_template/server_code.R
+++ plugins/development_template/server_code.R
@@ -7,107 +7,74 @@ m$code_%widget_id% <- ""
 shinyjs::delay(300, shinyjs::runjs("var event = new Event('resize'); window.dispatchEvent(event);"))
 
 # Comment code
-observeEvent(input$code_%widget_id%_comment, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$code_comment"))
-
-    tryCatch({
-        toggle_comments(
-            id = id, input_id = "code_%widget_id%", code = input$code_%widget_id%,
-            selection = input$code_%widget_id%_comment$range, session = session
-        )
-    }, error = function(e) cat(paste0("\\n", now(), " - widget %widget_id% - error = ", toString(e))))
-})
+observeEvent(input$code_%widget_id%_comment, try_catch("input$code_%widget_id%_comment", {
+    
+    toggle_comments(
+        id = id, input_id = "code_%widget_id%", code = input$code_%widget_id%,
+        selection = input$code_%widget_id%_comment$range, session = session
+    )
+}))
 
 # Run all code with shortcut
-observeEvent(input$code_%widget_id%_run_all, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$code_run_all"))
-
+observeEvent(input$code_%widget_id%_run_all, try_catch("input$code_%widget_id%_run_all", {
+    
     if ("projects_widgets_console" %in% user_accesses){
         m$code_%widget_id% <- input$code_%widget_id%
         shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
     }
-})
+}))
 
 # Run code when button is clicked
-observeEvent(input$display_figure_%widget_id%, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$display_figure"))
+observeEvent(input$display_figure_%widget_id%, try_catch("input$display_figure_%widget_id%", {
     
-    tryCatch({
+    # If current selected tab is figure settings when run code button is clicked, generate code from these settings
+    if (length(input$current_tab_%widget_id%) == 0) current_tab <- "figure_settings"
+    else current_tab <- input$current_tab_%widget_id%
     
-        # If current selected tab is figure settings when run code button is clicked, generate code from these settings
-        if (length(input$current_tab_%widget_id%) == 0) current_tab <- "figure_settings"
-        else current_tab <- input$current_tab_%widget_id%
+    if (current_tab == "figure_settings"){
         
-        if (current_tab == "figure_settings"){
-            
-            # Code to generate code from figure settings
-            
-            # ...
-            code <- ""
-            
-            # Update ace editor with generated code
-            shinyAce::updateAceEditor(session, "code_%widget_id%", value = code)
-            
-            m$code_%widget_id% <- code
-            
-            shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
-        }
-        
-        # Check if user has access
-        else if ("projects_widgets_console" %in% user_accesses){
-            m$code_%widget_id% <- input$code_%widget_id%
-            shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
-        }
-        
-    }, error = function(e){
-        show_message_bar(id, output, "error_displaying_figure", "severeWarning", i18n = i18np, ns = ns)
-        cat(paste0("\\n", now(), " - widget %widget_id% - input$display_figure - error = ", toString(e)))
-    })
-})
-
-# Run code at patient update
-# observeEvent(m$selected_person, {
-    # %req%
-    # if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer m$selected_person"))
-    
-    # if (isTRUE(input$run_code_on_data_update_%widget_id%)) shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
-# })
-
-# Run code at visit_detail update
-# observeEvent(m$selected_visit_detail, {
-    # %req%
-    # if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer m$selected_person"))
-    
-    # if (isTRUE(input$run_code_on_data_update_%widget_id%)) shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
-# })
-
-# Run code
-observeEvent(input$run_code_%widget_id%, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$run_code"))
-    
-    tryCatch({
-        
-        # Put here the code to execute when the "Run code" button is clicked
+        # Code to generate code from figure settings
         
         # ...
+        code <- ""
         
-        # Go to figure tab
-        if (!input$figure_and_settings_side_by_side_%widget_id%) shinyjs::click("figure_button_%widget_id%")
+        # Update ace editor with generated code
+        shinyAce::updateAceEditor(session, "code_%widget_id%", value = code)
         
-    }, error = function(e){
-        show_message_bar(id, output, "error_displaying_figure", "severeWarning", i18n = i18np, ns = ns)
-        cat(paste0("\\n", now(), " - widget %widget_id% - input$display_figure - error = ", toString(e)))
-    })
-})
+        m$code_%widget_id% <- code
+        
+        shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
+    }
+    
+    # Check if user has access
+    else if ("projects_widgets_console" %in% user_accesses){
+        m$code_%widget_id% <- input$code_%widget_id%
+        shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
+    }
+}))
+
+# Run code at patient update
+# observeEvent(m$selected_person, try_catch("m$selected_person", {
+    # if (isTRUE(input$run_code_on_data_update_%widget_id%)) shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
+# }))
+
+# Run code at visit_detail update
+# observeEvent(m$selected_visit_detail, try_catch("m$selected_visit_detail", {
+    # if (isTRUE(input$run_code_on_data_update_%widget_id%)) shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-run_code_%widget_id%', Math.random());"))
+# }))
+
+# Run code
+observeEvent(input$run_code_%widget_id%, try_catch("(input$run_code_%widget_id%", {
+        
+    # Put here the code to execute when the "Run code" button is clicked
+    
+    # ...
+    
+    # Go to figure tab
+    if (!input$figure_and_settings_side_by_side_%widget_id%) shinyjs::click("figure_button_%widget_id%")
+}))
 
 # Save code with shortcut
-observeEvent(input$code_%widget_id%_save, {
-    %req%
-    if (debug) cat(paste0("\\n", now(), " - mod_", id, " - widget_id = %widget_id% - observer input$code_save"))
-    
+observeEvent(input$code_%widget_id%_save, try_catch("input$code_%widget_id%_save", {
     shinyjs::runjs(paste0("Shiny.setInputValue('", id, "-save_params_and_code_%widget_id%', Math.random());"))
-})
+}))
