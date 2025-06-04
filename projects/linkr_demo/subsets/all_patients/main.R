0a7f5b081390f563cd8362f48f40028256b44f4f
Jane Doe <boris.delange@proton.me>
2025-06-04 20:41:49 +0200

    Update subset code

--- projects/linkr_demo/subsets/all_patients/main.R
+++ projects/linkr_demo/subsets/all_patients/main.R
@@ -1,5 +1,3 @@
-add_patients_to_subset(
-    patients = d$visit_detail %>% dplyr::select(person_id, visit_occurrence_id, visit_detail_id),
-    subset_id = %subset_id%,
-    output = output, r = r, m = m
-)
+patients <- d$visit_detail %>% dplyr::select(person_id, visit_occurrence_id, visit_detail_id)
+
+add_patients_to_subset(patients)
